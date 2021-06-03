dbf95d6fc848a3f566c10f21821ef199d6bfec98
livelace <o.popov@livelace.ru>
2021-06-03 21:11:28 +0300

    home path replacement

--- assets/gosquito_provision.sh
+++ assets/gosquito_provision.sh
@@ -7,12 +7,13 @@ echo "WARNING:"
 #-----------------------------------------------------------------------------
 
 SESSION_NAME="$1"
-SET_REPO="$2"
+SOURCE_REPO="$2"
 SET_NAME="$3"
-SET_TEMP="/tmp/source"
+
+SOURCE_TEMP="/tmp/source"
+SET_DIR="${SOURCE_TEMP}/set/${SET_NAME}"
 
 TARGET_PATH="/home/user/.gosquito"
-TARGET_FLOW_DIR="${TARGET_PATH}/flow"
 TARGET_PLUGIN_DIR="${TARGET_PATH}/plugin"
 
 CONFIG_FILE="config.toml"
@@ -27,36 +28,37 @@ WEBCHELA_SERVER="${SESSION_NAME}-webchela"
 #-----------------------------------------------------------------------------
 
 function copy_flow() {
-  for flow in $(cat $1);do
+  flow_file="$1"
+
+  for flow in $(cat $flow_file);do
     flow_dir=$(dirname "$flow")
-    target_flow_dir="${TARGET_FLOW_DIR}/${flow_dir}"
+    target_flow_dir="${TARGET_PATH}/${flow_dir}"
 
     mkdir -p "${target_flow_dir}"
 
     echo "INFO: Copy flow: ${flow}"
-    cp "${SET_TEMP}/flow/$flow" "${target_flow_dir}/"
+    cp "${SOURCE_TEMP}/$flow" "${target_flow_dir}/"
   done
 }
 
 #-----------------------------------------------------------------------------
 
-if [[ ! "$SESSION_NAME" || ! "$SET_REPO" || ! "$SET_NAME" ]];then
+if [[ ! "$SESSION_NAME" || ! "$SOURCE_REPO" || ! "$SET_NAME" ]];then
   echo "ERROR: Usage $0 supersession https://github.com/username/gosquito-source-set.git superset"
   exit 1
 fi
 
 #-----------------------------------------------------------------------------
 
-mkdir -p "${TARGET_FLOW_DIR}" || (echo "ERROR: Cannot create dir: ${TARGET_FLOW_DIR}" && exit 1)
 mkdir -p ${TARGET_PLUGIN_DIR}/{data,state,temp} || (echo "ERROR: Cannot create dir: ${TARGET_PLUGIN_DIR}" && exit 1)
 
 #-----------------------------------------------------------------------------
 
-rm -rf "$SET_TEMP"
+rm -rf "$SOURCE_TEMP"
 
-git clone "$SET_REPO" "$SET_TEMP" || (echo "ERROR: Cannot clone repo: ${SET_REPO}" && exit 1)
+git clone "$SOURCE_REPO" "$SOURCE_TEMP" || (echo "ERROR: Cannot clone repo: ${SOURCE_REPO}" && exit 1)
 
-cd "${SET_TEMP}/set/${SET_NAME}" || (echo "ERROR: Cannot find set: ${SET_REPO}" && exit 1)
+cd "$SET_DIR" || (echo "ERROR: Cannot find set: ${SOURCE_REPO}" && exit 1)
 
 #-----------------------------------------------------------------------------
 
@@ -71,8 +73,8 @@ cp "${CONFIG_FILE}" "${TARGET_PATH}/"
 
 copy_flow "$FLOW_FILE"
 
-for flow_group_file in $(cat $FLOW_GROUP_FILE);do
-  copy_flow "flow_group/${flow_group_file}"
+for group_file in $(cat "$FLOW_GROUP_FILE");do
+  copy_flow "${SOURCE_TEMP}/${group_file}"
 done
 
 #-----------------------------------------------------------------------------
