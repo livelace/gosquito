1710711e5d5662f00938c409c1a2a33959d188c0
livelace <o.popov@livelace.ru>
2021-07-24 15:54:15 +0300

    jte

---
+++ pipeline_config.groovy
@@ -0,0 +1,18 @@
+jte {
+    pipeline_template = 'k8s_build.groovy'
+}
+
+libraries {
+    git {
+        repo_url = 'https://github.com/livelace/gosquito.git'
+    }
+    go {
+        options = '-tags dynamic github.com/livelace/gosquito/cmd/gosquito'
+    }
+    mattermost
+    version
+}
+
+keywords {
+    build_image = 'harbor-core.k8s-2.livelace.ru/dev/gobuild:latest'
+}
