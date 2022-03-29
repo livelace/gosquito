9247eb1682008e12f4f30e13c6e55076afe3d85c
Oleg 'livelace' Popov <o.popov@livelace.ru>
2022-03-29 23:58:34 +0300

    dracut test

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -8,11 +8,11 @@ libraries {
         source = "${APP_NAME}"
         destination = "${APP_NAME}-${APP_VERSION}.appimage"
     }
-/*     dependency_check
+    dependency_check
     dependency_track {
         project = "${APP_NAME}"
         version = env.VERSION
-    } */
+    }
     dracut
     git {
         repo_url = "${APP_REPO}"
@@ -22,9 +22,9 @@ libraries {
         options = "-ldflags=\"-X 'github.com/livelace/gosquito/pkg/gosquito/core.APP_VERSION=${APP_VERSION}'\" " +
             "-tags dynamic github.com/livelace/gosquito/cmd/gosquito"
     }
-/*     harbor_replicate {
+    harbor_replicate {
         policy = "${APP_NAME}"
-    } */
+    }
     k8s_build {
         image = "harbor-core.k8s-2.livelace.ru/dev/gosquito:${IMAGE_TAG}"
         force_pull = true
@@ -36,10 +36,10 @@ libraries {
     kaniko {
         destination = "data/${APP_NAME}:${IMAGE_TAG}"
     }
-/*     mattermost
+    mattermost
     nexus {
         source = "${APP_NAME}-${APP_VERSION}.appimage"
         destination = "dists-internal/${APP_NAME}/${APP_NAME}-${APP_VERSION}.appimage"
     }
-    sonarqube */
+    sonarqube
 }
