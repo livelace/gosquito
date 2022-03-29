b8e7ae11b142f734816679d447fdf835a28e8da0
Oleg 'livelace' Popov <o.popov@livelace.ru>
2022-03-30 02:07:09 +0300

    ignore user account absence

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -4,7 +4,7 @@ def APP_VERSION = env.VERSION + '-${GIT_COMMIT_SHORT}'
 def IMAGE_TAG = env.VERSION == "master" ? "latest" : env.VERSION
 
 libraries {
-    /* appimage {
+    appimage {
         source = "${APP_NAME}"
         destination = "${APP_NAME}-${APP_VERSION}.appimage"
     }
@@ -12,7 +12,7 @@ libraries {
     dependency_track {
         project = "${APP_NAME}"
         version = env.VERSION
-    } */
+    }
     dracut
     git {
         repo_url = "${APP_REPO}"
@@ -22,9 +22,9 @@ libraries {
         options = "-ldflags=\"-X 'github.com/livelace/gosquito/pkg/gosquito/core.APP_VERSION=${APP_VERSION}'\" " +
             "-tags dynamic github.com/livelace/gosquito/cmd/gosquito"
     }
-    /* harbor_replicate {
+    harbor_replicate {
         policy = "${APP_NAME}"
-    } */
+    }
     k8s_build {
         image = "harbor-core.k8s-2.livelace.ru/dev/gosquito:${IMAGE_TAG}"
         force_pull = true
@@ -36,10 +36,10 @@ libraries {
     kaniko {
         destination = "data/${APP_NAME}:${IMAGE_TAG}"
     }
-    /* mattermost
+    mattermost
     nexus {
         source = "${APP_NAME}-${APP_VERSION}.appimage"
         destination = "dists-internal/${APP_NAME}/${APP_NAME}-${APP_VERSION}.appimage"
     }
-    sonarqube */
+    sonarqube
 }
