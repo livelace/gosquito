885f86f2388a8b90feab3b10d10c49c686db61fe
Oleg 'livelace' Popov <o.popov@livelace.ru>
2024-04-04 05:05:44 +0300

    ci: refactor

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -8,7 +8,7 @@ libraries {
         source = "${APP_NAME}"
         destination = "${APP_NAME}-${APP_VERSION}.appimage"
     }
-    dependency_check
+    //dependency_check
     //dependency_track {
     //    project = "${APP_NAME}"
     //    version = env.VERSION
@@ -22,20 +22,20 @@ libraries {
         options = "-ldflags=\"-X 'github.com/livelace/gosquito/pkg/gosquito/core.APP_VERSION=${APP_VERSION}'\" " +
             "-tags dynamic github.com/livelace/gosquito/cmd/gosquito"
     }
-    harbor_replicate {
-        policy = "${APP_NAME}"
-    }
+    //harbor_replicate {
+    //    policy = "${APP_NAME}"
+    //}
     k8s_build {
-        image = "harbor-core.k8s-2.livelace.ru/dev/gosquito:${IMAGE_TAG}"
+        image = "registry.livelace.ru/dev/gosquito:${IMAGE_TAG}"
         force_pull = true
     }
     kaniko {
-        destination = "data/${APP_NAME}:${IMAGE_TAG}"
+        destination = "infra/${APP_NAME}:${IMAGE_TAG}"
     }
-    mattermost
-    nexus {
-        source = "${APP_NAME}-${APP_VERSION}.appimage"
-        destination = "dists-internal/${APP_NAME}/${APP_NAME}-${APP_VERSION}.appimage"
-    }
-    sonarqube
+    //mattermost
+    //nexus {
+    //    source = "${APP_NAME}-${APP_VERSION}.appimage"
+    //    destination = "dists-internal/${APP_NAME}/${APP_NAME}-${APP_VERSION}.appimage"
+    //}
+    //sonarqube
 }
